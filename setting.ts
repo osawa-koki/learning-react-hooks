f216de323097a2a81e5a5eefe1d6c4772adf6e4e
osawa-koki <github@osawa.dev>
2023-07-13 13:02:39 +0900

    プロジェクトの初期化。

---
+++ setting.ts
@@ -0,0 +1,11 @@
+import Env from './next.config.js'
+const isProd = process.env.NODE_ENV === 'production'
+
+const setting = {
+  isProd,
+  basePath: Env.basePath,
+  apiPath: isProd ? '' : 'http://localhost:8000',
+  title: '👺👺👺 Learning React-Hooks 👺👺👺'
+}
+
+export default setting
